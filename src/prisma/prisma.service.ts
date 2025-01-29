319cafaf9f780a9050d0c780b35ca825fe222ed8
Thành Nguyễn <thanhnguyen@ThanhNC40s-MacBook-Air.local>
2025-01-29 11:49:34 +0700

    Update key

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,6 @@ export class PrismaService extends PrismaClient {
     super({
       datasources: {
         db: {
-          // url: 'postgresql://postgres:ThanhNC40@FPT@localhost:5434/testdb?schema=public'
           url: configService.get('DATABASE_URL'),
         },
       },
